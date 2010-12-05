c96e257904c73dd26c521fbf2a22888329f34235
Jonas Nockert <jonasnockert@gmail.com>
2010-12-05 18:48:51 +0100

    URL field for comments now support local URLs, e.g. without TLDs

--- methodiki/customcomments/forms.py
+++ methodiki/customcomments/forms.py
@@ -1,5 +1,8 @@
 # -*- coding: utf-8 -*-
+import re
+
 from django.contrib.comments.forms import CommentForm
+from django.core.validators import URLValidator
 
 
 class CustomCommentForm(CommentForm):
@@ -11,3 +14,28 @@ class CustomCommentForm(CommentForm):
         self.fields['url'].widget.attrs['class'] = class_str
         self.fields['comment'].widget.attrs['class'] = "span-10 append-8 " \
                                                        "last input"
+        # Replace standard URL validation to support local URLs,
+        # e.g. without TLDs.
+        self.fields['url'].validators = [IntranetURLValidator()]
+
+
+class IntranetURLValidator(URLValidator):
+    """ URL validation that support local URLs, e.g. without TLDs. """
+
+    def __init__(self):
+        super(IntranetURLValidator, self).__init__()
+
+        # FIXME: For now, don't assume URLs are reachable
+        self.verify_exists = False
+
+        # Pattern that only matches web URLs — http, https, and things like
+        # "www.example.com"
+        # See http://daringfireball.net/2010/07/improved_regex_for_matching_urls
+        self.regex = re.compile(ur"(?i)\b((?:https?://|www\d{0,3}[.]|" \
+                                ur"[a-z0-9.\-]+[.][a-z]{2,4}/)(?:[^\s()<>]+|" \
+                                ur"\(([^\s()<>]+|" \
+                                ur"(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|" \
+                                ur"(\([^\s()<>]+\)))*\)|" \
+                                ur"[^\s`!()\[\]{};:'" \
+                                ur'"' \
+                                ur".,<>?«»“”‘’]))")
